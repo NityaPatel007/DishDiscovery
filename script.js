1cec5a18deca72e65356739d006abed3a56a9734
Nitya Patel <nityapatel0771@gmail.com>
2024-02-02 23:18:28 -0500

    Added

---
+++ script.js
@@ -0,0 +1,27 @@
+const images = ['italianpasta.jpg', 'back.jpg', 'Pancakes.jpg', 'Poha.jpg'];
+        let currentIndex = 0;
+
+        function changeBackground() {
+            const heroSlider = document.querySelector('.hero-slider');
+            const heroText = document.querySelector('.hero-text');
+            const currentImage = images[currentIndex];
+
+            heroSlider.style.backgroundImage = `url('${currentImage}')`;
+
+            // Set text color based on the current image
+            const textColor = getTextcolorForImage(currentImage);
+            heroText.style.color = textColor;
+
+            currentIndex = (currentIndex + 1) % images.length;
+        }
+
+        function getTextcolorForImage(image) {
+            // Check the current image and return the appropriate text color
+            if (image === 'italianpasta.jpg' || image === 'back.jpg') {
+                return 'black';
+            } else if (image === 'Pancakes.jpg' || image === 'Poha.jpg') {
+                return 'white';
+            }
+        }
+
+        setInterval(changeBackground, 5000);
